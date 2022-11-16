0xhsn/connect

commit 5ade29b2c334774ebe0625f7154471fc2fae6ee9

linting fixes (#309)
* linting fixes

* fixes

src/actions/files.js
@@ -3,7 +3,7 @@ import { athena as Athena, devices as Devices, raw as Raw } from '@commaai/api';
 
 import { updateDeviceOnline, fetchDeviceNetworkStatus } from '.';
 import * as Types from './types';
-import { deviceOnCellular, getDeviceFromState, deviceVersionAtLeast } from '../utils';
+import { deviceOnCellular, getDeviceFromState, deviceVersionAtLeast, asyncSleep } from '../utils';
 
 const FILE_NAMES = {
   qcameras: 'qcamera.ts',
@@ -28,7 +28,8 @@ function pathToFileName(dongleId, path) {
 async function athenaCall(dongleId, payload, sentryFingerprint, retryCount = 0) {
   try {
     while (openRequests > MAX_OPEN_REQUESTS) {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      // eslint-disable-next-line no-await-in-loop
+      await asyncSleep(2000);
     }
     openRequests += 1;
     const resp = await Athena.postJsonRpcPayload(dongleId, payload);
@@ -37,7 +38,7 @@ async function athenaCall(dongleId, payload, sentryFingerprint, retryCount = 0) 
   } catch (err) {
     openRequests -= 1;
     if (!err.resp && retryCount < MAX_RETRIES) {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await asyncSleep(2000);
       return athenaCall(dongleId, payload, sentryFingerprint, retryCount + 1);
     }
     if (err.message && (err.message.indexOf('Timed out') === -1
@@ -63,93 +64,19 @@ export async function fetchUploadUrls(dongleId, paths) {
   return null;
 }
 
-export function doUpload(dongleId, fileNames, paths, urls) {
-  return async (dispatch, getState) => {
-    const { device } = getState();
-    let loopedUploads = !deviceVersionAtLeast(device, '0.8.13');
-    if (!loopedUploads) {
-      const files_data = paths.map((path, i) => ({
-        fn: path,
-        url: urls[i],
-        headers: { 'x-ms-blob-type': 'BlockBlob' },
-        allow_cellular: false,
-      }));
-      const payload = {
-        id: 0,
-        jsonrpc: '2.0',
-        method: 'uploadFilesToUrls',
-        params: { files_data },
-        expiry: Math.floor(Date.now() / 1000) + (86400 * 7),
-      };
-      const resp = await athenaCall(dongleId, payload, 'action_files_athena_uploads');
-      if (resp && resp.error && resp.error.code === -32000
-        && resp.error.data.message === 'too many values to unpack (expected 3)') {
-        loopedUploads = true;
-      } else if (!resp || resp.error) {
-        const newUploading = {};
-        for (const fileName of fileNames) {
-          newUploading[fileName] = {};
-        }
-        dispatch(updateDeviceOnline(dongleId, Math.floor(Date.now() / 1000)));
-        dispatch(updateFiles(newUploading));
-      } else if (resp.offline) {
-        dispatch(updateDeviceOnline(dongleId, 0));
-      } else if (resp.result === 'Device offline, message queued') {
-        const newUploading = {};
-        for (const fileName of fileNames) {
-          newUploading[fileName] = { progress: 0, current: false };
-        }
-        dispatch(updateFiles(newUploading));
-      } else if (resp.result) {
-        if (resp.result.failed) {
-          const uploading = {};
-          for (const path of resp.result.failed) {
-            const idx = paths.indexOf(path);
-            if (idx !== -1) {
-              uploading[fileNames[idx]] = { notFound: true };
-            }
-          }
-          dispatch(updateFiles(uploading));
-        }
-        dispatch(fetchUploadQueue(dongleId));
-      }
-    }
-
-    if (loopedUploads) {
-      for (let i = 0; i < fileNames.length; i++) {
-        const payload = {
-          id: 0,
-          jsonrpc: '2.0',
-          method: 'uploadFileToUrl',
-          params: [paths[i], urls[i], { 'x-ms-blob-type': 'BlockBlob' }],
-          expiry: Math.floor(Date.now() / 1000) + (86400 * 7),
-        };
-        const resp = await athenaCall(dongleId, payload, 'files_actions_athena_upload');
-        if (!resp || resp.error) {
-          const uploading = {};
-          uploading[fileNames[i]] = {};
-          dispatch(updateDeviceOnline(dongleId, Math.floor(Date.now() / 1000)));
-          dispatch(updateFiles(uploading));
-        } else if (resp.offline) {
-          dispatch(updateDeviceOnline(dongleId, 0));
-        } else if (resp.result === 'Device offline, message queued') {
-          const uploading = {};
-          uploading[fileNames[i]] = { progress: 0, current: false };
-          dispatch(updateFiles(uploading));
-        } else if (resp.result === 404 || (resp.result && resp.result.failed && resp.result.failed[0] === paths[i])) {
-          const uploading = {};
-          uploading[fileNames[i]] = { notFound: true };
-          dispatch(updateFiles(uploading));
-        } else if (resp.result) {
-          dispatch(fetchUploadQueue(dongleId));
-        }
-      }
-    }
+export function updateFiles(files) {
+  return (dispatch, getState) => {
+    const { dongleId } = getState();
+    dispatch({
+      type: Types.ACTION_FILES_UPDATE,
+      dongleId,
+      files,
+    });
   };
 }
 
 export function fetchFiles(routeName, nocache = false) {
-  return async (dispatch, getState) => {
+  return async (dispatch) => {
     let files;
     try {
       files = await Raw.getRouteFiles(routeName, nocache);
@@ -161,55 +88,24 @@ export function fetchFiles(routeName, nocache = false) {
 
     const dongleId = routeName.split('|')[0];
     const urlName = routeName.replace('|', '/');
-    const res = {};
-    for (const type of Object.keys(FILE_NAMES)) {
-      if (files[type]) {
-        for (const file of files[type]) {
-          const segmentNum = parseInt(file.split(urlName)[1].split('/')[1], 10);
-          const fileName = `${routeName}--${segmentNum}/${type}`;
-          res[fileName] = {
-            url: file,
-          };
-        }
-      }
-    }
+    const urls = Object
+      .keys(FILE_NAMES)
+      .filter((type) => files[type])
+      .flatMap((type) => files[type].map((file) => ([type, file])))
+      .reduce((state, [type, file]) => {
+        const segmentNum = parseInt(file.split(urlName)[1].split('/')[1], 10);
+        const fileName = `${routeName}--${segmentNum}/${type}`;
+        state[fileName] = {
+          url: file,
+        };
+        return state;
+      }, {});
 
     dispatch({
       type: Types.ACTION_FILES_URLS,
       dongleId,
-      urls: res,
+      urls,
     });
-  };
-}
-
-export function fetchAthenaQueue(dongleId) {
-  return async (dispatch, getState) => {
-    let queue;
-    try {
-      queue = await Devices.getAthenaQueue(dongleId);
-    } catch (err) {
-      console.error(err);
-      Sentry.captureException(err, { fingerprint: 'action_files_fetch_athena_queue' });
-      return;
-    }
-
-    const newUploading = {};
-    for (const q of queue) {
-      if (!q.method || !q.expiry || q.expiry < Math.floor(Date.now() / 1000)) {
-        continue;
-      }
-
-      if (q.method === 'uploadFileToUrl') {
-        const fileName = pathToFileName(dongleId, q.params[0]);
-        newUploading[fileName] = { progress: 0, current: false };
-      } else if (q.method === 'uploadFilesToUrls') {
-        for (const { fn } of q.params.files_data) {
-          const fileName = pathToFileName(dongleId, fn);
-          newUploading[fileName] = { progress: 0, current: false };
-        }
-      }
-    }
-    dispatch(updateFiles(newUploading));
   };
 }
 
@@ -250,14 +146,14 @@ export function fetchUploadQueue(dongleId) {
     const device = getDeviceFromState(getState(), dongleId);
     const uploadingFiles = {};
     const newCurrentUploading = {};
-    for (const uploading of uploadQueue.result) {
+    uploadQueue.result.forEach((uploading) => {
       const urlParts = uploading.url.split('?')[0].split('/');
       const filename = urlParts[urlParts.length - 1];
       const segNum = urlParts[urlParts.length - 2];
       const datetime = urlParts[urlParts.length - 3];
-      const dongleId = urlParts[urlParts.length - 4];
+      const dongle = urlParts[urlParts.length - 4];
       const type = Object.entries(FILE_NAMES).find((e) => e[1] == filename)[0];
-      const fileName = `${dongleId}|${datetime}--${segNum}/${type}`;
+      const fileName = `${dongle}|${datetime}--${segNum}/${type}`;
       const waitingWifi = Boolean(deviceOnCellular(device) && uploading.allow_cellular === false);
       uploadingFiles[fileName] = {
         current: uploading.current,
@@ -272,8 +168,9 @@ export function fetchUploadQueue(dongleId) {
         paused: waitingWifi,
       };
       delete prevFilesUploading[uploading.id];
-    }
-    if (getState().dongleId === dongleId && Object.keys(prevFilesUploading).length) { // some item is done uploading
+    });
+    // some item is done uploading
+    if (getState().dongleId === dongleId && Object.keys(prevFilesUploading).length) {
       const routeName = Object.values(prevFilesUploading)[0].fileName.split('--').slice(0, 2).join('--');
       dispatch(fetchFiles(routeName, true));
     }
@@ -293,19 +190,124 @@ export function fetchUploadQueue(dongleId) {
   };
 }
 
-export function updateFiles(files) {
-  return (dispatch, getState) => {
-    const { dongleId } = getState();
-    dispatch({
-      type: Types.ACTION_FILES_UPDATE,
-      dongleId,
-      files,
-    });
+export function doUpload(dongleId, fileNames, paths, urls) {
+  return async (dispatch, getState) => {
+    const { device } = getState();
+    let loopedUploads = !deviceVersionAtLeast(device, '0.8.13');
+    if (!loopedUploads) {
+      const filesData = paths.map((path, i) => ({
+        fn: path,
+        url: urls[i],
+        headers: { 'x-ms-blob-type': 'BlockBlob' },
+        allow_cellular: false,
+      }));
+      const payload = {
+        id: 0,
+        jsonrpc: '2.0',
+        method: 'uploadFilesToUrls',
+        params: { files_data: filesData },
+        expiry: Math.floor(Date.now() / 1000) + (86400 * 7),
+      };
+      const resp = await athenaCall(dongleId, payload, 'action_files_athena_uploads');
+      if (resp && resp.error && resp.error.code === -32000
+        && resp.error.data.message === 'too many values to unpack (expected 3)') {
+        loopedUploads = true;
+      } else if (!resp || resp.error) {
+        const newUploading = fileNames.reduce((state, fn) => {
+          state[fn] = {};
+          return state;
+        }, {});
+        dispatch(updateDeviceOnline(dongleId, Math.floor(Date.now() / 1000)));
+        dispatch(updateFiles(newUploading));
+      } else if (resp.offline) {
+        dispatch(updateDeviceOnline(dongleId, 0));
+      } else if (resp.result === 'Device offline, message queued') {
+        const newUploading = fileNames.reduce((state, fn) => {
+          state[fn] = { progress: 0, current: false };
+          return state;
+        });
+        dispatch(updateFiles(newUploading));
+      } else if (resp.result) {
+        if (resp.result.failed) {
+          const uploading = resp.result.failed
+            .filter((path) => paths.indexOf(path) > -1)
+            .reduce((state, path) => {
+              const fn = fileNames[paths.indexOf(path)];
+              state[fn] = { notFound: true };
+              return state;
+            });
+          dispatch(updateFiles(uploading));
+        }
+        dispatch(fetchUploadQueue(dongleId));
+      }
+    }
+
+    if (loopedUploads) {
+      for (let i = 0; i < fileNames.length; i++) {
+        const payload = {
+          id: 0,
+          jsonrpc: '2.0',
+          method: 'uploadFileToUrl',
+          params: [paths[i], urls[i], { 'x-ms-blob-type': 'BlockBlob' }],
+          expiry: Math.floor(Date.now() / 1000) + (86400 * 7),
+        };
+        const resp = await athenaCall(dongleId, payload, 'files_actions_athena_upload');
+        if (!resp || resp.error) {
+          const uploading = {};
+          uploading[fileNames[i]] = {};
+          dispatch(updateDeviceOnline(dongleId, Math.floor(Date.now() / 1000)));
+          dispatch(updateFiles(uploading));
+        } else if (resp.offline) {
+          dispatch(updateDeviceOnline(dongleId, 0));
+        } else if (resp.result === 'Device offline, message queued') {
+          const uploading = {};
+          uploading[fileNames[i]] = { progress: 0, current: false };
+          dispatch(updateFiles(uploading));
+        } else if (resp.result === 404 || resp?.result?.failed[0] === paths[i]) {
+          const uploading = {};
+          uploading[fileNames[i]] = { notFound: true };
+          dispatch(updateFiles(uploading));
+        } else if (resp.result) {
+          dispatch(fetchUploadQueue(dongleId));
+        }
+      }
+    }
+  };
+}
+
+export function fetchAthenaQueue(dongleId) {
+  return async (dispatch) => {
+    let queue;
+    try {
+      queue = await Devices.getAthenaQueue(dongleId);
+    } catch (err) {
+      console.error(err);
+      Sentry.captureException(err, { fingerprint: 'action_files_fetch_athena_queue' });
+      return;
+    }
+
+    const newUploading = {};
+    for (const q of queue) {
+      if (!q.method || !q.expiry || q.expiry < Math.floor(Date.now() / 1000)) {
+        continue;
+      }
+
+      if (q.method === 'uploadFileToUrl') {
+        const fileName = pathToFileName(dongleId, q.params[0]);
+        newUploading[fileName] = { progress: 0, current: false };
+      } else if (q.method === 'uploadFilesToUrls') {
+        for (const { fn } of q.params.files_data) {
+          const fileName = pathToFileName(dongleId, fn);
+          newUploading[fileName] = { progress: 0, current: false };
+        }
+      }
+    }
+    dispatch(updateFiles(newUploading));
   };
 }
 
 export function cancelUploads(dongleId, ids) {
-  return async (dispatch, getState) => {
+  return async (dispatch) => {
     const payload = {
       id: 0,
       jsonrpc: '2.0',

.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     ],
     'react/prop-types': 0,
     'react/require-default-props': 0,
+    'no-await-in-loop': 'warn',
     'no-plusplus': [
       'error',
       {

src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
 import qs from 'query-string';
 import localforage from 'localforage';
-import * as Sentry from "@sentry/react";
+import * as Sentry from '@sentry/react';
 
 import { CircularProgress, Grid } from '@material-ui/core';
 
@@ -112,8 +112,8 @@ class App extends Component {
       return this.renderLoading();
     }
 
-    const showLogin = !MyCommaAuth.isAuthenticated() && !isDemo() && !getZoom(window.location.pathname) &&
-      !getClipsNav(window.location.pathname)?.clip_id;
+    const showLogin = !MyCommaAuth.isAuthenticated() && !isDemo() && !getZoom(window.location.pathname)
+      && !getClipsNav(window.location.pathname)?.clip_id;
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>

src/__puppeteer__/demo.test.js
@@ -1,21 +1,18 @@
 /* eslint-env jest */
 import puppeteer from 'puppeteer';
+import { asyncSleep } from '../utils';
 
 const width = 1600;
 const height = 1200;
 
 jest.setTimeout(60000);
 
-async function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 describe('demo mode', () => {
   let browser;
   let page;
 
   beforeEach(async () => {
-    await delay(500);
+    await asyncSleep(500);
   });
   beforeAll(async () => {
     browser = await puppeteer.launch({
@@ -31,7 +28,7 @@ describe('demo mode', () => {
     });
     await page.goto('http://localhost:3003/4cf7a6ad03080c90');
     // wait for the data to start loading...
-    await delay(8000);
+    await asyncSleep(8000);
 
     return true;
   });
@@ -46,7 +43,7 @@ describe('demo mode', () => {
     expect((await list.$$(':scope > a')).length).toBe(1);
 
     await expect(page).toClick('.DriveEntry');
-    await delay(3000);
+    await asyncSleep(3000);
 
     const video = await page.$('video');
     const videoSrc = await page.evaluate((vid) => vid.getAttribute('src'), video);

src/actions/cached.js
@@ -11,7 +11,7 @@ const eventsRequests = {};
 const coordsRequests = {};
 const driveCoordsRequests = {};
 let hasExpired = false;
-let cacheDB = null;
+const cacheDB = null;
 
 async function getCacheDB() {
   if (cacheDB !== null) {
@@ -68,13 +68,13 @@ async function getCacheDB() {
       const driveCoordsStore = db.createObjectStore('driveCoords', { keyPath: 'key' });
       driveCoordsStore.createIndex('key', 'key', { unique: true });
       driveCoordsStore.createIndex('expiry', 'expiry', { unique: false });
-    }
+    };
   });
 }
 
-async function getCacheItem(store, key, version=undefined) {
+async function getCacheItem(store, key, version = undefined) {
   if (!hasExpired) {
-    setTimeout(() => expireCacheItems(store), 5000);  // TODO: better expire time
+    setTimeout(() => expireCacheItems(store), 5000); // TODO: better expire time
     hasExpired = true;
   }
 
@@ -95,10 +95,10 @@ async function getCacheItem(store, key, version=undefined) {
       }
     };
     req.onerror = (ev) => reject(ev.target.error);
-  })
+  });
 }
 
-async function setCacheItem(store, key, expiry, data, version=undefined) {
+async function setCacheItem(store, key, expiry, data, version = undefined) {
   const db = await getCacheDB();
   if (!db) {
     return null;
@@ -114,7 +114,7 @@ async function setCacheItem(store, key, expiry, data, version=undefined) {
   return new Promise((resolve, reject) => {
     req.onsuccess = (ev) => resolve(ev.target.result);
     req.onerror = (ev) => reject(ev.target.error);
-  })
+  });
 }
 
 async function expireCacheItems(store) {
@@ -126,10 +126,10 @@ async function expireCacheItems(store) {
   const transaction = db.transaction([store], 'readwrite');
   const objStore = transaction.objectStore(store);
 
-  const idx = IDBKeyRange.upperBound(parseInt(Date.now()/1000));
+  const idx = IDBKeyRange.upperBound(Math.floor(Date.now() / 1000));
   const req = objStore.index('expiry').openCursor(idx);
   req.onsuccess = (ev) => {
-    let cursor = ev.target.result;
+    const cursor = ev.target.result;
     if (cursor) {
       objStore.delete(cursor.primaryKey);
       cursor.continue();
@@ -329,17 +329,17 @@ export function fetchEvents(route) {
     });
     resolveEvents(driveEvents);
     if (!USE_LOCAL_EVENTS_DATA) {
-      setCacheItem('events', route.fullname, parseInt(Date.now()/1000) + (86400*14), driveEvents, route.maxqlog);
+      setCacheItem('events', route.fullname, Math.floor(Date.now() / 1000) + (86400 * 14), driveEvents, route.maxqlog);
     }
-  }
+  };
 }
 
 export function fetchLocations(route) {
   return (dispatch, getState) => {
     dispatch(fetchCoord(route, [route.start_lng, route.start_lat], 'startLocation'));
     dispatch(fetchCoord(route, [route.end_lng, route.end_lat], 'endLocation'));
-  }
-};
+  };
+}
 
 export function fetchCoord(route, coord, locationKey) {
   return async (dispatch, getState) => {
@@ -403,8 +403,8 @@ export function fetchCoord(route, coord, locationKey) {
       location,
     });
     resolveLocation(location);
-    setCacheItem('coords', coord, parseInt(Date.now()/1000) + (86400*14), location);
-  }
+    setCacheItem('coords', coord, Math.floor(Date.now() / 1000) + (86400 * 14), location);
+  };
 }
 
 export function fetchDriveCoords(route) {
@@ -431,7 +431,7 @@ export function fetchDriveCoords(route) {
         type: Types.ACTION_UPDATE_ROUTE,
         fullname: route.fullname,
         route: {
-          driveCoords: driveCoords,
+          driveCoords,
         },
       });
       return;
@@ -480,26 +480,24 @@ export function fetchDriveCoords(route) {
       return;
     }
 
-    driveCoords = driveCoords.reduce((prev, curr) => {
-      return {
-        ...prev,
-        ...curr.reduce((p, cs) => {
-          p[cs.t] = [cs.lng, cs.lat];
-          return p;
-        }, {}),
-      }
-    }, {});
+    driveCoords = driveCoords.reduce((prev, curr) => ({
+      ...prev,
+      ...curr.reduce((p, cs) => {
+        p[cs.t] = [cs.lng, cs.lat];
+        return p;
+      }, {}),
+    }), {});
 
     dispatch({
       type: Types.ACTION_UPDATE_ROUTE,
       fullname: route.fullname,
       route: {
-        driveCoords: driveCoords,
+        driveCoords,
       },
     });
     resolveDriveCoords(driveCoords);
     if (!USE_LOCAL_COORDS_DATA) {
-      setCacheItem('driveCoords', route.fullname, parseInt(Date.now()/1000) + (86400*14), driveCoords, route.maxqlog);
+      setCacheItem('driveCoords', route.fullname, Math.floor(Date.now() / 1000) + (86400 * 14), driveCoords, route.maxqlog);
     }
-  }
+  };
 }

src/actions/clips.js
@@ -3,7 +3,7 @@ import { push } from 'connected-react-router';
 import MyCommaAuth from '@commaai/my-comma-auth';
 import { clips as Clips } from '@commaai/api';
 
-import { checkRoutesData, selectDevice, urlForState } from './';
+import { checkRoutesData, selectDevice, urlForState } from '.';
 import { getClipsNav } from '../url';
 import * as Types from './types';
 
@@ -157,7 +157,7 @@ export function fetchClipsDetails(clip_id) {
     } catch (err) {
       if (err.resp && err.resp.status === 404) {
         if (!MyCommaAuth.isAuthenticated()) {
-          window.location = `/?r=${encodeURI(window.location.pathname)}`;  // redirect to login
+          window.location = `/?r=${encodeURI(window.location.pathname)}`; // redirect to login
         } else {
           dispatch({
             type: Types.ACTION_CLIPS_ERROR,

src/actions/history.js
@@ -1,13 +1,13 @@
 import { LOCATION_CHANGE } from 'connected-react-router';
 import { getDongleID, getZoom, getPrimeNav, getClipsNav } from '../url';
 import { primeNav, selectDevice, selectRange } from './index';
-import { clipsExit, fetchClipsDetails, fetchClipsList } from  './clips';
+import { clipsExit, fetchClipsDetails, fetchClipsList } from './clips';
 
 export const onHistoryMiddleware = ({ dispatch, getState }) => (next) => (action) => {
   if (action.type === LOCATION_CHANGE && ['POP', 'REPLACE'].includes(action.payload.action)) {
     const state = getState();
 
-    next(action);  // must be first, otherwise breaks history
+    next(action); // must be first, otherwise breaks history
 
     const pathDongleId = getDongleID(action.payload.location.pathname);
     if (pathDongleId && pathDongleId !== state.dongleId) {
@@ -37,4 +37,4 @@ export const onHistoryMiddleware = ({ dispatch, getState }) => (next) => (action
   } else {
     next(action);
   }
-}
+};

src/actions/history.test.js
@@ -1,4 +1,5 @@
 /* eslint-env jest */
+/* eslint-disable no-import-assign */
 import { routerMiddleware, LOCATION_CHANGE } from 'connected-react-router';
 import thunk from 'redux-thunk';
 
@@ -9,19 +10,19 @@ import * as actionsIndex from './index';
 const create = (initialState) => {
   const store = {
     getState: jest.fn(() => initialState),
-    dispatch: jest.fn()
-  }
-  const next = jest.fn()
+    dispatch: jest.fn(),
+  };
+  const next = jest.fn();
 
-  const middleware = (store) => (next) => (action) => {
-    routerMiddleware(history)(store)(next)(action);
-    onHistoryMiddleware(store)(next)(action);
-    thunk(store)(next)(action);
-  }
-  const invoke = action => middleware(store)(next)(action)
+  const middleware = (s) => (n) => (action) => {
+    routerMiddleware(history)(s)(n)(action);
+    onHistoryMiddleware(s)(n)(action);
+    thunk(s)(n)(action);
+  };
+  const invoke = (action) => middleware(store)(next)(action);
 
-  return { store, next, invoke }
-}
+  return { store, next, invoke };
+};
 
 describe('history middleware', () => {
   it('passes through non-function action', () => {
@@ -41,8 +42,8 @@ describe('history middleware', () => {
   it('passes dispatch and getState', () => {
     const { store, invoke } = create();
     invoke((dispatch, getState) => {
-      dispatch('TEST DISPATCH')
-      getState()
+      dispatch('TEST DISPATCH');
+      getState();
     });
     expect(store.dispatch).toHaveBeenCalledWith('TEST DISPATCH');
   });

commit 35286dadaa0e052e5fa21e1539bb40029c84dc85

fix typo
introduced in 5ade29b2c334774ebe0625f7154471fc2fae6ee9

src/actions/files.js
@@ -263,7 +263,7 @@ export function doUpload(dongleId, fileNames, paths, urls) {
           const uploading = {};
           uploading[fileNames[i]] = { progress: 0, current: false };
           dispatch(updateFiles(uploading));
-        } else if (resp.result === 404 || resp?.result?.failed[0] === paths[i]) {
+        } else if (resp.result === 404 || resp?.result?.failed?.[0] === paths[i]) {
           const uploading = {};
           uploading[fileNames[i]] = { notFound: true };
           dispatch(updateFiles(uploading));